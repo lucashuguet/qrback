4f8318093dc0cbb069060ae24c92f5881dd62441
AstroGoat <lucashuguet31@gmail.com>
2025-08-06 18:27:51 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "cli tool to backup files as qr codes";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { nixpkgs, ... }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        openssl pkg-config cargo-nextest cargo-expand
+        qrencode zbar
+      ];
+    };
+  };
+}
